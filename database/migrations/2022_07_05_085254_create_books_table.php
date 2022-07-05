fa6c9df876826744b46a0182734c634dd22dd464
muhammad taufik zamaludin <taufikmuhammad550@gmail.com>
2022-07-05 16:15:33 +0700

    migration books

---
+++ database/migrations/2022_07_05_085254_create_books_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->integer('location_id');
+            $table->integer('type_id');
+            $table->string('name');
+            $table->longText('deskripsi')->nullable();
+            $table->dateTime('tanggal_rilis')->nullable();
+            $table->integer('total_page')->nullable();
+            $table->longText('penerbit')->nullable();
+            $table->longText('penulis')->nullable();
+            $table->longText('book_photo')->nullable();
+            $table->longText('book_photo_path')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
