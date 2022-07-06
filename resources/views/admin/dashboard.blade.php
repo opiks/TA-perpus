c03f58427cbecd6bc3e42d38c5c81050e07f1908
muhammad taufik zamaludin <taufikmuhammad550@gmail.com>
2022-07-06 14:25:23 +0700

    dashboard sementara

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.adminlayout')
-<!-- 
+<!--
 =============================================
    Nama : Muhamad Taufik Zamaludin
    NIM : 1120031026
@@ -7,8 +7,8 @@
    Nama : Muhamad Zulfiqor
    NIM : 1120031025
 
-   Deskripsi Program : App Perpustakaan 
-============================================= 
+   Deskripsi Program : App Perpustakaan
+=============================================
 -->
 
 @section('title')
@@ -25,35 +25,27 @@
         <div class="col-xl-12">
             <!-- BEGIN row -->
             <div class="row">
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <div class="card mb-3 overflow-hidden fs-13px border-0 bg-gradient-custom-orange">
                         <div class="card-body position-relative">
-                            <h5 class="text-white-transparent-8 mb-3 fs-16px">Page Views</h5>
-                            <h3 class="text-white mt-n1">320.4k</h3>
+                            <h5 class="text-white-transparent-8 mb-3 fs-16px">Total Member</h5>
+                            <h3 class="text-white mt-n1">10</h3>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <div class="card mb-3 overflow-hidden fs-13px border-0 bg-gradient-custom-teal">
                         <div class="card-body position-relative">
-                            <h5 class="text-white-transparent-8 mb-3 fs-16px">Page Views</h5>
-                            <h3 class="text-white mt-n1">320.4k</h3>
+                            <h5 class="text-white-transparent-8 mb-3 fs-16px">Total Buku</h5>
+                            <h3 class="text-white mt-n1">1000</h3>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <div class="card mb-3 overflow-hidden fs-13px border-0 bg-gradient-custom-pink">
                         <div class="card-body position-relative">
-                            <h5 class="text-white-transparent-8 mb-3 fs-16px">Page Views</h5>
-                            <h3 class="text-white mt-n1">320.4k</h3>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-3">
-                    <div class="card mb-3 overflow-hidden fs-13px border-0 bg-gradient-custom-indigo">
-                        <div class="card-body position-relative">
-                            <h5 class="text-white-transparent-8 mb-3 fs-16px">Page Views</h5>
-                            <h3 class="text-white mt-n1">320.4k</h3>
+                            <h5 class="text-white-transparent-8 mb-3 fs-16px">Buku Dipinjam</h5>
+                            <h3 class="text-white mt-n1">10</h3>
                         </div>
                     </div>
                 </div>
